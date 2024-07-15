85e3067f7da67dd766d2443644f888489af434c1
harshithsai218 <harshithsai2004@gmail.com>
2024-07-15 23:44:22 +0530

    num

--- app.js
+++ app.js
@@ -11,8 +11,18 @@ const app=express();
 
 app.use(bodyParser.json())
 
-app.use('/api/places',placesRoutes);
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  
+    next();
+  });
 
+app.use('/api/places',placesRoutes);
 app.use('/api/users',usersRoutes);
 
 app.use((req,res,next)=>{
@@ -29,7 +39,7 @@ app.use((error,req,res,next)=>{
 });
 
 mongoose
-    .connect('mongodb+srv://MERN:141312@mern.qepcug5.mongodb.net/places?retryWrites=true&w=majority&appName=MERN')
+    .connect('mongodb+srv://MERN:141312@mern.qepcug5.mongodb.net/mern_places?retryWrites=true&w=majority&appName=MERN')
     .then(()=>{
         app.listen(5000);
     })
