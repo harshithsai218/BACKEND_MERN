d0b197d31364e5827dcef719c0c4705cb68aa2ca
harshithsai218 <harshithsai2004@gmail.com>
2024-07-22 12:24:28 +0530

    fileUpload

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,13 +1,16 @@
 const express = require ("express");
 const { check } = require("express-validator");
 
-const userControllers = require('../controllers/users-controllers')
+const userControllers = require('../controllers/users-controllers');
+const fileUpload = require('../MiddleWare/file-Upload')
 
 const router =express()
 
 router.get('/',userControllers.getUsers);
 
-router.post('/signup',[
+router.post('/signup',
+    fileUpload.single('image'),
+    [
         check('name').not().isEmpty(),
         check('email').normalizeEmail().isEmail(),
         check('password').isLength({min:8 })
